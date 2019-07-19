ac1820abdc313dd08340cb2ecd57068e6e2ced67
Jean-François Côté <jcote@stingray.com>
2019-07-19 13:42:50 -0400

    Fix #12 : Add basic website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,14 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'OpenAPI Style Validator', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  url: 'https://openapitools.github.io/openapi-style-validator/',
+  baseUrl: '/openapi-style-validator/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'openapi-style-validator',
+  organizationName: 'OpenAPITools',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -73,7 +70,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} OpenAPI Tools`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
