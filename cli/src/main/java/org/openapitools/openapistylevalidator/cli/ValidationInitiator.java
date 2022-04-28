6b4b05ba5dbae9bd5ff42506f187c3c1a55a7a28
Jean-François Côté <jcote@stingray.com>
2022-04-28 14:34:03 -0400

    Refactoring of the cli modules and cleanup the unit tests (#337)
    * Refactoring of the cli modules and cleanup in the logs following hierarchical unit tests

---
+++ cli/src/main/java/org/openapitools/openapistylevalidator/cli/ValidationInitiator.java
@@ -0,0 +1,34 @@
+package org.openapitools.openapistylevalidator.cli;
+
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import org.apache.commons.cli.CommandLine;
+import org.eclipse.microprofile.openapi.models.OpenAPI;
+import org.openapitools.empoa.swagger.core.internal.SwAdapter;
+import org.openapitools.openapistylevalidator.OpenApiSpecStyleValidator;
+import org.openapitools.openapistylevalidator.ValidatorParameters;
+import org.openapitools.openapistylevalidator.styleerror.StyleError;
+
+import java.util.List;
+
+public class ValidationInitiator {
+    public List<StyleError> validate(OptionManager optionManager, CommandLine commandLine) {
+        OpenAPI openAPI = parseToOpenAPIModels(optionManager, commandLine);
+
+        OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
+
+        ValidatorParameters parameters = optionManager.getOptionalValidatorParametersOrDefault(commandLine);
+        return openApiSpecStyleValidator.validate(parameters);
+    }
+
+    private OpenAPI parseToOpenAPIModels(OptionManager optionManager, CommandLine commandLine) {
+        OpenAPIParser openApiParser = new OpenAPIParser();
+        ParseOptions parseOptions = new ParseOptions();
+        parseOptions.setResolve(true);
+
+        SwaggerParseResult parserResult = openApiParser.readLocation(optionManager.getSource(commandLine), null, parseOptions);
+        io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI = parserResult.getOpenAPI();
+        return SwAdapter.toOpenAPI(swaggerOpenAPI);
+    }
+}
---
+++ cli/src/test/java/org/openapitools/openapistylevalidator/cli/ResourceTest.java
@@ -0,0 +1,116 @@
+package org.openapitools.openapistylevalidator.cli;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.openapitools.openapistylevalidator.ValidatorParameters;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResourceTest {
+    private static final String PREFIX = "    \"";
+    private static final String SEPARATOR = "\": ";
+    private static final String SEPARATOR_QUOTE = "\": \"";
+    private static final String NEXT_LINE = "," + System.lineSeparator();
+    private static final String NEXT_LINE_QUOTE = "\"," + System.lineSeparator();
+
+    @Test
+    void defaultJsonFileContainsConstants() throws Exception {
+        InputStream stream = getClass().getResourceAsStream("/default.json");
+        assert stream != null;
+        String content = IOUtils.toString(stream, StandardCharsets.UTF_8);
+
+        String sb = "{" +
+                System.lineSeparator() +
+                PREFIX +
+                ValidatorParameters.VALIDATE_INFO_LICENSE +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_INFO_DESCRIPTION +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_INFO_CONTACT +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_OPERATION_OPERATION_ID +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_OPERATION_DESCRIPTION +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_OPERATION_TAG +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_OPERATION_SUMMARY +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_MODEL_PROPERTIES_EXAMPLE +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_MODEL_PROPERTIES_DESCRIPTION +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_MODEL_REQUIRED_PROPERTIES +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_MODEL_NO_LOCAL_DEF +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.VALIDATE_NAMING +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.IGNORE_HEADER_X_NAMING +
+                SEPARATOR +
+                "true" +
+                NEXT_LINE +
+                PREFIX +
+                ValidatorParameters.PATH_NAMING_CONVENTION +
+                SEPARATOR_QUOTE +
+                ValidatorParameters.NamingConvention.HyphenCase +
+                NEXT_LINE_QUOTE +
+                PREFIX +
+                ValidatorParameters.HEADER_NAMING_CONVENTION +
+                SEPARATOR_QUOTE +
+                ValidatorParameters.NamingConvention.UnderscoreUpperCase +
+                NEXT_LINE_QUOTE +
+                PREFIX +
+                ValidatorParameters.PARAMETER_NAMING_CONVENTION +
+                SEPARATOR_QUOTE +
+                ValidatorParameters.NamingConvention.CamelCase +
+                NEXT_LINE_QUOTE +
+                PREFIX +
+                ValidatorParameters.PROPERTY_NAMING_CONVENTION +
+                SEPARATOR_QUOTE +
+                ValidatorParameters.NamingConvention.CamelCase +
+                "\"" +
+                System.lineSeparator() +
+                "}";
+        assertEquals(sb, content);
+    }
+}
