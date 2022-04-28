8986d49bd36d1c284ccdcbf7d00df40aef6dc7a8
jcote <jcote@stingray.com>
2022-04-28 11:14:09 -0400

    Fix to use log4j2 properly

--- cli/src/main/java/org/openapitools/openapistylevalidator/cli/OptionManager.java
+++ cli/src/main/java/org/openapitools/openapistylevalidator/cli/OptionManager.java
@@ -6,15 +6,15 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openapitools.openapistylevalidator.ValidatorParameters;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 class OptionManager {
-    private static final Logger logger = LoggerFactory.getLogger(OptionManager.class);
+    private static final Logger logger = LogManager.getLogger(OptionManager.class);
 
     private static final String SOURCE_OPT_SHORT = "s";
     private static final String SOURCE_OPT_LONG = "source";
--- cli/src/main/java/org/openapitools/openapistylevalidator/cli/OutputUtils.java
+++ cli/src/main/java/org/openapitools/openapistylevalidator/cli/OutputUtils.java
@@ -2,9 +2,9 @@ package org.openapitools.openapistylevalidator.cli;
 
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openapitools.openapistylevalidator.styleerror.StyleError;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Properties;
 
 class OutputUtils {
-    private static final Logger logger = LoggerFactory.getLogger(OutputUtils.class);
+    private static final Logger logger = LogManager.getLogger(OutputUtils.class);
 
     void printHelp(final Options options, final String cmdLineSyntax) {
         try (StringWriter stringWriter = new StringWriter(); PrintWriter printWriter = new PrintWriter(stringWriter)) {
