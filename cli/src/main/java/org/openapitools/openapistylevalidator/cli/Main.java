ba1c7bb072c315e88d7c8ed6c56cf4c9bafea681
jcote <jcote@stingray.com>
2022-04-28 14:22:37 -0400

    Refactoring of the cli modules and cleanup in the logs following hierarchical unit tests

--- cli/src/main/java/org/openapitools/openapistylevalidator/cli/Main.java
+++ cli/src/main/java/org/openapitools/openapistylevalidator/cli/Main.java
@@ -1,16 +1,6 @@
 package org.openapitools.openapistylevalidator.cli;
 
-import io.swagger.parser.OpenAPIParser;
-import io.swagger.v3.parser.core.models.ParseOptions;
-import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.apache.commons.cli.*;
-
-import org.openapitools.empoa.swagger.core.internal.SwAdapter;
-import org.openapitools.openapistylevalidator.OpenApiSpecStyleValidator;
-import org.openapitools.openapistylevalidator.ValidatorParameters;
-import org.openapitools.openapistylevalidator.styleerror.StyleError;
-
-import java.util.List;
 
 public class Main {
 
@@ -20,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         OptionManager optionManager = new OptionManager(outputUtils);
+        ValidationInitiator validationInitiator = new ValidationInitiator();
 
         try {
             Options options = optionManager.getOptions();
@@ -33,7 +24,8 @@ public class Main {
                 } else if (optionManager.isVersionRequested(commandLine)) {
                     outputUtils.printVersion();
                 } else if (optionManager.isSourceProvided(commandLine)) {
-                    validateAndPrint(optionManager, commandLine);
+
+                    outputUtils.printResults(validationInitiator.validate(optionManager, commandLine));
                 } else {
                     outputUtils.printRequestError();
                 }
@@ -42,24 +34,4 @@ public class Main {
             outputUtils.printRequestError();
         }
     }
-
-    private static void validateAndPrint(OptionManager optionManager, CommandLine commandLine) {
-        outputUtils.printResults(validate(optionManager, commandLine));
-    }
-
-    static List<StyleError> validate(OptionManager optionManager, CommandLine commandLine) {
-        OpenAPIParser openApiParser = new OpenAPIParser();
-        ParseOptions parseOptions = new ParseOptions();
-        parseOptions.setResolve(true);
-
-        SwaggerParseResult parserResult = openApiParser.readLocation(optionManager.getSource(commandLine), null, parseOptions);
-        io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI = parserResult.getOpenAPI();
-
-        org.eclipse.microprofile.openapi.models.OpenAPI openAPI = SwAdapter.toOpenAPI(swaggerOpenAPI);
-        OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
-
-        ValidatorParameters parameters = optionManager.getOptionalValidatorParametersOrDefault(commandLine);
-        return openApiSpecStyleValidator.validate(parameters);
-    }
-
 }
